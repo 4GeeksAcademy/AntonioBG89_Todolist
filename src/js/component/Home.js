73c2fe988ec161ab8054b7c582a949c2935c8ffe
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-08-06 09:37:25 +0000

    Espectacular final

--- src/js/component/Home.js
+++ src/js/component/Home.js
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import ToDoComponent from "./ToDoComponent";
-import { FaRegTimesCircle } from "react-icons/fa";
 
 //create your first component
 const Home = (props) => {
 	const [todo, setTodo] = useState("");
 	const [todos, setTodos] = useState([]);
-	const [icon, setIcon] = useState("");
+
+	const deleteToDo = (index) => {
+		const newTodos = todos.filter((_, i) => i !== index);
+		setTodos(newTodos);
+	}
 
 	return (
 		<div className="container text-center mt-5">
 			<div className="container justify-content-center">
 
 				<div className="mb-3">
-					<label for="toDoList" className="form-label">TODO's</label>
-					<input type="text" className="form-control" id="toDolist" placeholder="What needs to be done?" value={todo} onKeyDown={(e) => {
-						if (e.key === "Enter") {
-							setTodos([...todos, todo]);
+					<label htmlFor="toDoList" className="form-label">TODO's</label>
+					<input
+						type="text"
+						className="form-control"
+						id="toDolist" placeholder="What needs to be done?"
+						value={todo}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								setTodos([...todos, todo]);
 							setTodo("");
-						}
-					}} onChange={(e) => {
-						setTodo(e.target.value);
-					}} />
+							}
+						}} onChange={(e) => {
+							setTodo(e.target.value);
+						}} />
 				</div>
 
 				<div className="container mb-5">
-				{todos.map((todo) => {
-						return <div className="container estiloListaTodos d-flex justify-content-between" >{todo}<FaRegTimesCircle /></div>;
-					})}
-
+				{todos.map((todo,index) => (
+						<ToDoComponent
+							key={index}
+							todo={todo}
+							onDelete={()=> deleteToDo(index)}
+						/>
+					))}
 				</div >
 
 				<button className="col-1 btn btn-danger" onClick={() => {
--- src/js/component/ToDoComponent.js
+++ src/js/component/ToDoComponent.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { FaRegTimesCircle } from 'react-icons/fa';
 
-
-const ToDoComponent = () => {
+const ToDoComponent = ({ todo, onDelete }) => {
   return (
-    <div>{todo}</div>
-  )
-}
+    <div className="container estiloListaTodos d-flex justify-content-between" >
+      {todo}
+      <FaRegTimesCircle onClick={onDelete} style={{ cursor: 'pointer' }} />
+      </div>
+  );
+};
 
-export default ToDoComponent
+export default ToDoComponent;
